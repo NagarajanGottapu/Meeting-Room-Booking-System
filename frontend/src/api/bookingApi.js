36ab159e56e8f553e449e8faa69a801b6f44d5fd
NagarajanGottapu <avinashgottapu165@gmail.com>
2025-09-16 22:34:04 +0530

    Add files via upload

---
+++ frontend/src/api/bookingApi.js
@@ -0,0 +1,26 @@
+import api from './config';
+
+export const bookingApi = {
+  createBooking: async (bookingData) => {
+    const response = await api.post('/bookings', bookingData);
+    return response.data;
+  },
+
+  getUserBookings: async (userId) => {
+    const response = await api.get(`/bookings/user/${userId}`);
+    return response.data;
+  },
+
+  getRoomBookings: async (roomId, date) => {
+    const response = await api.get(`/bookings/room/${roomId}`, {
+      params: { date }
+    });
+    return response.data;
+  },
+
+  cancelBooking: async (bookingId, userId) => {
+  const response = await api.put(`/bookings/cancel/${bookingId}`, { userId });
+  return response.data;}
+
+
+};
